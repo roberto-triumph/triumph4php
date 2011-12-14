bee9fc272c10088dec83e335f6574bf4f3fd069d
Roberto Perpuly <robertop2004@gmail.com>
2011-12-13 20:13:05 -0800

    removed unneeded #include

--- src/windows/EditColorsPanelClass.cpp
+++ src/windows/EditColorsPanelClass.cpp
@@ -23,7 +23,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 #include <windows/EditColorsPanelClass.h>
-#include <widgets/CodeControlClass.h>
+#include <wx/stc/stc.h>
 
 mvceditor::EditColorsPanelClass::EditColorsPanelClass(wxWindow* parent, mvceditor::CodeControlOptionsClass& options)
 		: EditColorsPanelGeneratedClass(parent)
