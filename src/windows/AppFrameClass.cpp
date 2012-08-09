f5f103cc49e1f0978e25232bead1ea74651eb472
Roberto Perpuly <robertop2004@gmail.com>
2012-08-08 18:12:50 -0700

    fixed bug that was introduced last commits; outline notebook did not have a parent

--- src/windows/AppFrameClass.cpp
+++ src/windows/AppFrameClass.cpp
@@ -84,6 +84,8 @@ mvceditor::AppFrameClass::AppFrameClass(const std::vector<mvceditor::PluginClass
 		).PaneBorder(true).Gripper(false).Floatable(false).Resizable(true));
 	AuiManager.AddPane(ToolsNotebook, wxAuiPaneInfo().Bottom().Caption(
 		_("Tools")).Floatable(false).MinSize(-1, 260).Hide().Layer(1));
+	AuiManager.AddPane(OutlineNotebook, wxAuiPaneInfo().Left().Caption(
+        _("Outlines")).Floatable(false).MinSize(260, -1).Hide());
 	
 	AuiManager.AddPane(ToolBar, wxAuiPaneInfo().Top(
 		).CaptionVisible(false).CloseButton(false).Gripper(
