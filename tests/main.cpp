d881b60c51bf12dfe9f3d9fc3decf7ca132e0108
Roberto Perpuly <robertop2004@gmail.com>
2012-10-06 09:40:04 -0700

    updated to compile on gcc version 4.6.3
    new version gave the errror "va_list has not been declared"

--- tests/main.cpp
+++ tests/main.cpp
@@ -22,6 +22,7 @@
  * @copyright  2009-2011 Roberto Perpuly
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  */
+#include <stdarg.h>
 #include <UnitTest++.h>
 #include <TestReporterStdout.h>
 #include <TestRunner.h>
