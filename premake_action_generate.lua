8e59936f09f40d718bcb900b6a8f0784fa71edca
Roberto Perpuly <robertop2004@gmail.com>
2012-10-20 09:36:56 -0700

    update generate action to make the new parsers

--- premake_action_generate.lua
+++ premake_action_generate.lua
@@ -19,7 +19,7 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 -- THE SOFTWARE.
 --
--- @copyright  2009-2011 Roberto Perpuly
+-- @copyright  2009-2012 Roberto Perpuly
 -- @license    http://www.opensource.org/licenses/mit-license.php The MIT License
 -------------------------------------------------------------------
 
@@ -32,32 +32,115 @@ newaction {
 		-- regenerate the parser and lexer implementation files
 		-- re2c does not escape windows paths and leads to compile errors, turn off debug info
 		cmd = "re2c -c -i --no-generation-date " ..
-			"-o " .. normalizepath("src/language/Php53LexicalAnalyzerImpl.cpp") .. " " ..
-			normalizepath("src/language/Php53LexicalAnalyzerImpl.re");
+			"-o " .. normalizepath("lib/pelet/src/Php53LexicalAnalyzer.cpp") .. " " ..
+			normalizepath("lib/pelet/src/Php53LexicalAnalyzer.re");
 		code = os.execute(cmd) 
 		if code ~= 0 then
-			print("re2c command failed for file src/language/Php53LexicalAnalyzerImpl.re. Is re2c installed? Is it in the PATH?");	
+			print("re2c command failed for file lib/pelet/src/Php53LexicalAnalyzer.re. Is re2c installed? Is it in the PATH?");	
 		end
 		cmd = "re2c -c -i --no-generation-date " ..
-			"-o " .. normalizepath("src/language/LanguageDiscoveryClass.cpp") .. " " ..
-			normalizepath("src/language/LanguageDiscoveryClass.re");
+			"-o " .. normalizepath("lib/pelet/src/Php54LexicalAnalyzer.cpp") .. " " ..
+			normalizepath("lib/pelet/src/Php54LexicalAnalyzer.re");
 		code = os.execute(cmd) 
 		if code ~= 0 then
-			print("re2c command failed for file src/language/LanguageDiscoveryClass.re. Is re2c installed? Is it in the PATH?");	
+			print("re2c command failed for file lib/pelet/src/Php54LexicalAnalyzer.re. Is re2c installed? Is it in the PATH?");	
 		end
 		cmd = "re2c -c -i --no-generation-date " ..
-			"-o " .. normalizepath("src/language/SqlLexicalAnalyzerClass.cpp") .. " " ..
-			normalizepath("src/language/SqlLexicalAnalyzerClass.re");
+			"-o " .. normalizepath("lib/pelet/src/LanguageDiscoveryClass.cpp") .. " " ..
+			normalizepath("lib/pelet/src/LanguageDiscoveryClass.re");
 		code = os.execute(cmd) 
 		if code ~= 0 then
-			print("re2c command failed for file src/language/SqlLexicalAnalyzerClass.re. Is re2c installed? Is it in the PATH?");	
+			print("re2c command failed for file lib/pelet/src/LanguageDiscoveryClass.re. Is re2c installed? Is it in the PATH?");	
 		end
-		cmd = "bison --no-lines --warnings=error --defines " ..
-			"-o " .. normalizepath("src/language/Php53ParserImpl.cpp") .. " " ..
-			normalizepath("src/language/Php53ParserImpl.y")
+		code = os.execute(cmd) 
+		if os.is("windows") then
+			-- in windows line macros dont work correctly
+			cmd = "bison --no-lines --warnings=error " ..
+				" -o " .. normalizepath("lib/pelet/src/Php53FullParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php53FullParser.y")
+		else 
+			cmd = "bison  --warnings=error " ..
+				" -o " .. normalizepath("lib/pelet/src/Php53FullParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php53FullParser.y")
+		end
 		code = os.execute(cmd) 
 		if code ~= 0 then
-			print("Bison command failed for file src/language/Php53ParserImpl.y. Is bison installed? Is it in the PATH?");	
+			print("Bison command failed for file lib/pelet/src/Php53FullParser.y. Is bison installed? Is it in the PATH?");	
+		end
+		
+		if os.is("windows") then
+			-- in windows line macros dont work correctly
+			cmd = "bison --no-lines --warnings=error " ..
+				" -o " .. normalizepath("lib/pelet/src/Php53LintParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php53LintParser.y")
+		else 
+			cmd = "bison  --warnings=error " ..
+				" -o " .. normalizepath("lib/pelet/src/Php53LintParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php53LintParser.y")
+		end
+		code = os.execute(cmd) 
+		if code ~= 0 then
+			print("Bison command failed for file lib/pelet/src/Php53LintParser.y. Is bison installed? Is it in the PATH?");	
+		end
+		
+		if os.is("windows") then
+			-- in windows line macros dont work correctly
+			cmd = "bison --no-lines --warnings=error "  ..
+				" -o " .. normalizepath("lib/pelet/src/Php54FullParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php54FullParser.y")
+		else
+			cmd = "bison --warnings=error "  ..
+				" -o " .. normalizepath("lib/pelet/src/Php54FullParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php54FullParser.y")
+		end
+		code = os.execute(cmd) 
+		if code ~= 0 then
+			print("Bison command failed for file lib/pelet/src/Php54FullParser.y. Is bison installed? Is it in the PATH?");	
+		end
+		
+		if os.is("windows") then
+			-- in windows line macros dont work correctly
+			cmd = "bison --no-lines --warnings=error "  ..
+				" -o " .. normalizepath("lib/pelet/src/Php54LintParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php54LintParser.y")
+		else
+			cmd = "bison --warnings=error "  ..
+				" -o " .. normalizepath("lib/pelet/src/Php54LintParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php54LintParser.y")
+		end
+		code = os.execute(cmd) 
+		if code ~= 0 then
+			print("Bison command failed for file lib/pelet/src/Php54LintParser.y. Is bison installed? Is it in the PATH?");	
+		end
+		
+		if os.is("windows") then
+			-- in windows line macros dont work correctly
+			cmd = "bison --warnings=error " ..
+				" -o " .. normalizepath("lib/pelet/src/Php53ResourceParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php53ResourceParser.y")
+		else 
+			cmd = "bison  --warnings=error " ..
+				" -o " .. normalizepath("lib/pelet/src/Php53ResourceParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php53ResourceParser.y")
+		end
+		code = os.execute(cmd) 
+		if code ~= 0 then
+			print("Bison command failed for file lib/pelet/src/Php53ResourceParser.y. Is bison installed? Is it in the PATH?");	
+		end
+		
+		if os.is("windows") then
+			-- in windows line macros dont work correctly
+			cmd = "bison --warnings=error " ..
+				" -o " .. normalizepath("lib/pelet/src/Php54ResourceParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php54ResourceParser.y")
+		else 
+			cmd = "bison  --warnings=error " ..
+				" -o " .. normalizepath("lib/pelet/src/Php54ResourceParser.cpp") .. " " ..
+				normalizepath("lib/pelet/src/Php54ResourceParser.y")
+		end
+		code = os.execute(cmd) 
+		if code ~= 0 then
+			print("Bison command failed for file lib/pelet/src/Php54ResourceParser.y. Is bison installed? Is it in the PATH?");	
 		end
 	end
 }
