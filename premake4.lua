6c5618d844fb2a02c554606e10df1d28a0856155
Roberto Perpuly <robertop2004@gmail.com>
2013-07-21 14:07:36 -0700

    bad include dirs were being created during the build process

--- premake4.lua
+++ premake4.lua
@@ -208,11 +208,9 @@ solution "mvc-editor"
 		language "C++"
 		kind "WindowedApp"
 		files { "src/**.cpp", "src/**.h", "*.lua", "README.md" }
-		configuration { "vs2008" }
-			files { "src/mvceditor.rc" }
 		includedirs { "src/", "lib/keybinder/include/", "lib/pelet/include" }
 		links { "tests", "keybinder", "pelet" }
-
+		
 		sociconfiguration()
 		configuration "Debug"
 			pickywarnings(_ACTION)
@@ -226,10 +224,12 @@ solution "mvc-editor"
 			wxconfiguration("Release", _ACTION)
 			wxappconfiguration("Release", _ACTION)
 		configuration { "Debug", "vs2008" }
+			files { "src/mvceditor.rc" }
 			postbuildcommands { "cd " .. normalizepath("Debug") .. " && tests.exe --all" }
 		configuration { "Debug", "gmake or codelite" }
 			postbuildcommands { "cd " .. normalizepath("Debug") .. " && ./tests --all" }
 		configuration { "Release", "vs2008" }
+			files { "src/mvceditor.rc" }
 			postbuildcommands { "cd " .. normalizepath("Release") .. " && tests.exe --all"  }
 		configuration { "Release", "gmake or codelite" }
 			postbuildcommands { "cd " .. normalizepath("Release") .. " && ./tests --all" }
