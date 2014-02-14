53e386e31e2a372c36520f8f6f07ccb6a2b964b9
Roberto Perpuly <robertop2004@gmail.com>
2014-02-13 22:35:12 -0800

    updates to debian build script
    remove unused files, create proper license, version file

--- premake_action_dist.lua
+++ premake_action_dist.lua
@@ -47,29 +47,65 @@ newaction {
 				os.execute(cmd) 
 			end
 		else
-			workDir = normalizepath("../mvc-editor-0.6")
-			workLibDir = normalizepath("../mvc-editor-0.6/Release/libs")
+			workDir = path.getabsolute("../mvc-editor-0.6")
+			workLibDir = path.getabsolute("../mvc-editor-0.6/Release/libs")
 			rootDir = normalizepath("./")
 			libWildcards =  normalizepath("./Release/*.so*")
-			assetDir = "/usr/share/mvc-editor/assets"
+			assetDir = "/usr/share/mvc-editor"
 			branch = "master";
+			debianControl = path.getabsolute("../debian.control");
 			
-			os.execute(string.format("rm -rf %s", workDir))
-						
-			batchexecute(rootDir, {
-			
-				-- clone the project into a new, temp directory
-				string.format("git clone . %s", workDir),
-				string.format("cd %s", workDir),
-				string.format("git checkout %s", branch),
+			if (not os.isdir(workDir)) then
+				batchexecute(rootDir, {
 				
+					-- clone the project into a new, temp directory
+					string.format("git clone . %s", workDir),
+					string.format("cd %s", workDir),
+					"git submodule init",
+					"git submodule update lib/pelet",
+					
+						
+					-- create the makefiles, which will be used to build
+					-- the release version of the project
+					-- basically, this command makes the work directory use
+					-- the wxWidgets, SOCI installation in the main project dir (which
+					-- are already compiled, the work dir does not have them and we
+					-- want to avoid re-compiling them)
+					-- also, we generate the Makefiles in the root of the project
+					-- also, we want to set the assets dir to the final location
+					-- of the assets (in the system where the .deb file will be installed
+					-- ie the end user machine)
+					"MVCEDITOR_WXCONFIG=" .. normalizepath(WX_CONFIG) .. " " ..
+					"MVCEDITOR_SOCI_DEBUG_INCLUDE_DIR=" .. normalizepath(SOCI_DEBUG_INCLUDE_DIR) .. " " ..
+					"MVCEDITOR_SOCI_DEBUG_LIB_DIR=" .. normalizepath(SOCI_DEBUG_LIB_DIR)  .. " "  ..
+					"MVCEDITOR_SOCI_RELEASE_INCLUDE_DIR=" .. normalizepath(SOCI_RELEASE_INCLUDE_DIR) .. " " ..
+					"MVCEDITOR_SOCI_RELEASE_LIB_DIR=" .. normalizepath(SOCI_RELEASE_LIB_DIR) .. " " .. 
+					"MVCEDITOR_BUILD_SCRIPTS_DIR=. " .. 
+					"MVCEDITOR_LIB_DIR=" .. workLibDir .. " " ..
+					"MVCEDITOR_ASSET_DIR=" .. assetDir .. " " ..
+					"./premake4 gmake",
+					
+					-- this will prep the dir to be a debian work dir
+					"dh_make -s --email roberto@mvceditor.com  --native",
+					
+					-- remove any example files put there by dh_make
+					"rm -rf debian/*.ex",
+					"rm -rf debian/*.EX",
+				});
+			end
+			
+			
+			batchexecute(workDir, {
+				string.format("git checkout %s", branch),
+				"git submodule init",
+				"git submodule update lib/pelet",
 				
 				-- copy the compiled 3rd party dependencies into the work dir
+				-- remove any libs that were removed since the last time
+				-- this script ran
 				"install -d Release/libs",
-				string.format("cp -r %s %s", os.getcwd() .. "/Release/*.so*", "Release/libs"),
-				
-				"git submodule init",
-				"git submodule update lib/pelet"
+				"rm -fr Release/libs/*",
+				string.format("cp -r %s %s", os.getcwd() .. "/Release/*.so*", "Release/libs")
 			})
 			
 			-- get the version info from git and populate the version file
@@ -82,17 +118,6 @@ newaction {
 
 
 			batchexecute(workDir, {
-			
-				-- create the makefiles, which will be used to build
-				-- the release version of the project
-				-- basically, this command makes the work directory use
-				-- the wxWidgets, SOCI installation in the main project dir (which
-				-- are already compiled, the work dir does not have them and we
-				-- want to avoid re-compiling them)
-				-- also, we generate the Makefiles in the root of the project
-				-- also, we want to set the assets dir to the final location
-				-- of the assets (in the system where the .deb file will be installed
-				-- ie the end user machine)
 				"MVCEDITOR_WXCONFIG=" .. normalizepath(WX_CONFIG) .. " " ..
 				"MVCEDITOR_SOCI_DEBUG_INCLUDE_DIR=" .. normalizepath(SOCI_DEBUG_INCLUDE_DIR) .. " " ..
 				"MVCEDITOR_SOCI_DEBUG_LIB_DIR=" .. normalizepath(SOCI_DEBUG_LIB_DIR)  .. " "  ..
@@ -102,15 +127,14 @@ newaction {
 				"MVCEDITOR_LIB_DIR=" .. workLibDir .. " " ..
 				"MVCEDITOR_ASSET_DIR=" .. assetDir .. " " ..
 				"./premake4 gmake",
-				
-				-- this will prep the dir to be a debian work dir
-				"dh_make -s --email roberto@mvceditor.com  --native",
 
 				-- populate the install file, the file that tells which files to 
 				-- include in the .deb file
 				-- double quote the backslash since we have to escape the backslash in the shell
 				"echo 'Release/mvc-editor usr/bin\\n' > debian/install",
+				"echo 'version.txt usr/share/mvc-editor \\n' >> debian/install",
 				"find Release/libs -type f -name \"*\\\\.so*\" | awk '{ print $1  \" usr/lib/mvc-editor\" }' >> debian/install",
+				"find Release/libs -type l -name \"*\\\\.so*\" | awk '{ print $1  \" usr/lib/mvc-editor\" }' >> debian/install",
 
 				-- regex is complicated
 				-- find the basename from the asset file, then remove it
@@ -123,6 +147,17 @@ newaction {
 				-- mofiy the makefile to only build the main app not all of the examples, profilers, 
 				"sed -i 's/all: \$(PROJECTS)/all: mvc-editor/g' Makefile",
 				
+				-- modify the license file to be our own
+				"cat LICENSE > debian/copyright",
+				
+				-- remove unused debian package files
+				"rm -f debian/README.source",
+				"rm -f debian/README.debian",
+				
+				-- copy the debian control file, it gives the debian package
+				-- a description of our project
+				string.format("cat \"%s\" > debian/control", debianControl),
+				
 				-- finally, create the .deb this command will basically run our makefile
 				-- gather all of the 3rd party libs and assets and package them
 				-- we need to set the LD_LIBRAR_PATH so that dpkg finds our dependencies
@@ -130,6 +165,6 @@ newaction {
 				-- creates an unsigned version
 				"LD_LIBRARY_PATH=$LD_LIBRARY_PATH:Release/libs:Release dpkg-buildpackage -b -us -uc"
 			});
-		end	
+		end
 	end
 }
